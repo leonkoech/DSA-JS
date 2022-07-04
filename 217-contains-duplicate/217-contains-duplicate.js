985cffcc60fe510a8c26394e16a5adc16dfbf982
leon kipkoech <39020723+leonkoech@users.noreply.github.com>
2022-07-04 01:57:00 -0700

    Time: 99 ms (84.26%), Space: 51 MB (44.68%) - LeetHub

--- 217-contains-duplicate/217-contains-duplicate.js
+++ 217-contains-duplicate/217-contains-duplicate.js
@@ -3,14 +3,19 @@
  * @return {boolean}
  */
 var containsDuplicate = function(nums) {
-    var mySet = new Set()
-    for (var i=0; i<nums.length; i++){
-        if(mySet.has(nums[i])){
-            return true
-        }
+//     var mySet = new Set()
+//     for (var i=0; i<nums.length; i++){
+//         if(mySet.has(nums[i])){
+//             return true
+//         }
 
-        mySet.add(nums[i])
+//         mySet.add(nums[i])
         
-    }
-    return false
+//     }
+//     return false
+    
+    
+    var mySet = new Set(nums)
+    if(mySet.size===nums.length) return false
+    return true
 };
