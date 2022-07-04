973d300c9d9be27ac53665e0daf42ae45cf00961
leon kipkoech <39020723+leonkoech@users.noreply.github.com>
2022-07-04 10:22:17 -0700

    Time: 103 ms (23.85%), Space: 42.1 MB (49.49%) - LeetHub

--- 88-merge-sorted-array/88-merge-sorted-array.js
+++ 88-merge-sorted-array/88-merge-sorted-array.js
@@ -6,8 +6,8 @@
  * @return {void} Do not return anything, modify nums1 in-place instead.
  */
 var merge = function(nums1, m, nums2, n) {
-    for (var i=m+n-1;i>=m;i--){
-        nums1[i]=nums2.pop()
+    for (var i=0;i<n;i++){
+        nums1[m+i]=nums2[i]
     }
     nums1.sort((a,b)=>a-b)
     
