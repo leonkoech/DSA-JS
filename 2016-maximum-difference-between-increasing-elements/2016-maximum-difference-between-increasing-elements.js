f758ff9d3d23816c3e8741e42cb40e4de229a9d0
leon kipkoech <39020723+leonkoech@users.noreply.github.com>
2022-07-04 01:39:35 -0700

    Time: 106 ms (21.28%), Space: 42.6 MB (20.57%) - LeetHub

---
+++ 2016-maximum-difference-between-increasing-elements/2016-maximum-difference-between-increasing-elements.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var maximumDifference = function(nums) {
+    //pointer
+    var maxDiff=-1,
+        p1=0,
+        p2=1
+    while(p2<nums.length){
+        if(nums[p2]<nums[p1]){
+            p1=p2
+        }
+        else {
+            if(nums[p2]-nums[p1]>maxDiff){
+                    maxDiff=nums[p2]-nums[p1]
+            }
+        }
+        p2++
+    }
+    return maxDiff!=0?maxDiff:-1
+};
