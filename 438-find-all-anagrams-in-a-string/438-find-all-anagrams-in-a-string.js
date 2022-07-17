407023a3293d94e6c8ac72bf8317733bc9ac727d
leon kipkoech <39020723+leonkoech@users.noreply.github.com>
2022-07-17 01:32:50 -0700

    Time: 3234 ms (7.70%), Space: 52 MB (6.28%) - LeetHub

---
+++ 438-find-all-anagrams-in-a-string/438-find-all-anagrams-in-a-string.js
@@ -0,0 +1,46 @@
+/**
+ * @param {string} s
+ * @param {string} p
+ * @return {number[]}
+ */
+const checkAnagrams=(word1, word2)=>{
+    // create array[256] fill with zero
+    // all encounters in first word are added
+    // all encountered in second are subtracted
+    // array hould not contain anything greater than 0
+    
+    
+    var arr = new Array(256).fill(0)
+    
+    for(let i=0;i<word1.length;i++){
+        arr[word1[i].charCodeAt(0)-"a".charCodeAt(0)] ++
+        arr[word2[i].charCodeAt(0)-"a".charCodeAt(0)] --
+    }
+    
+    for(let i=0;i<arr.length;i++){
+        if(arr[i]>0){
+            return false
+        }
+    }
+    return true
+}
+
+var findAnagrams = function(s, p) {
+    // brute force 
+    // loop a window of sie p checking if that string is an anagram of p
+    // if so add o arr
+    
+    var fArr = [],
+        start=0,
+        end = p.length-1;
+    
+    while(end<s.length){
+        if(checkAnagrams(s.slice(start,end+1),p)){
+           
+            fArr.push(start)
+        }
+        end++
+        start++
+    }
+    return fArr
+};
