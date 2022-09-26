43b3205de2d6231a13c8f5eea53c5afd64896bf8
leon kipkoech <39020723+leonkoech@users.noreply.github.com>
2022-09-26 15:42:54 -0400

    Time: 108 ms (68.21%), Space: 42.5 MB (60.26%) - LeetHub

--- 1-two-sum/1-two-sum.js
+++ 1-two-sum/1-two-sum.js
@@ -4,18 +4,14 @@
  * @return {number[]}
  */
 var twoSum = function(nums, target) {
-    var finalArr = new Map()
     
-    for(let i=0;i<nums.length;i++){
-        var compliment = target - nums[i]
-        if(finalArr.has(compliment)){
-            return [finalArr.get(compliment), i]
-        }
-        else{
-            finalArr.set(nums[i],i)
-        }
-    }
-    return finalArr;
+    let records = {}
     
+   for (let i=0;i<nums.length;i++){
+       if(records[nums[i]] !== undefined){
+           return [i,records[nums[i]]]
+       }
+       records[target - nums[i]]=i
+   }
     
 };
