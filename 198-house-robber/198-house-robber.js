0e7e136251d7ac4daef97f14654d9211d4f96d61
leon kipkoech <39020723+leonkoech@users.noreply.github.com>
2022-09-29 13:45:58 -0400

    Time: 110 ms (14.39%), Space: 41.9 MB (54.35%) - LeetHub

---
+++ 198-house-robber/198-house-robber.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var rob = function(nums) {
+    // [rob1, rob2, n , n+1 ...]
+    let rob1 = 0, rob2 = 0
+    for (let n of nums){
+        let temp = Math.max(rob1+n,rob2)
+        rob1=rob2
+        rob2=temp
+    }
+    return rob2
+};
