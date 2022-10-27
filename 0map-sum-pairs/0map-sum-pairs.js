bfbe291cfab2e561895c5dfac2322668c3ca5b13
leon kipkoech <39020723+leonkoech@users.noreply.github.com>
2022-10-26 22:15:03 -0400

    Time: 93 ms, Memory: 43.7 MB - LeetHub

---
+++ 0map-sum-pairs/0map-sum-pairs.js
@@ -0,0 +1,62 @@
+
+var MapSum = function() {
+    this.root = {}
+};
+
+/** 
+ * @param {string} key 
+ * @param {number} val
+ * @return {void}
+ */
+MapSum.prototype.insert = function(key, val) {
+    // the key will be the index of the trie    // root will have an array
+    // the key will be a key // root will be a hashmap
+    this.root[key] = {}
+    let node = this.root[key]
+    for(let char of key){
+        if(!node[char]) {
+            node.val = val
+            node[char] = {}
+        }
+        node = node[char]
+    }
+    node.isEnd = true
+    
+    
+};
+
+MapSum.prototype.startsWith = function(prefix,key) {
+        let node = this.root[key]
+        for(let char of prefix){
+            if(!node[char]) {
+                return null
+            }
+            node=node[char]
+        }
+        return true
+}
+
+/** 
+ * @param {string} prefix
+ * @return {number}
+ */
+MapSum.prototype.sum = function(prefix) {
+    // loops through the root dict values
+    // performs a search
+    let node = this.root
+    let count = 0
+    for(let [key,val] of Object.entries(node)){
+            if(this.startsWith(prefix,key)){
+                
+                count+=parseInt(node[key].val)
+            }
+    }
+    return count
+};
+
+/** 
+ * Your MapSum object will be instantiated and called as such:
+ * var obj = new MapSum()
+ * obj.insert(key,val)
+ * var param_2 = obj.sum(prefix)
+ */
